0a37abd9332e3a02a87e62523eeb5b01f25e1cd8
Matt Haggard <haggardii@gmail.com>
2016-07-27 16:19:37 -0600

    Math-based expressions now work in inputs

--- buckets/web/static/js/react-money.jsx
+++ buckets/web/static/js/react-money.jsx
@@ -1,3 +1,11 @@
+var _groupregex = new RegExp(',', "g");
+math.config({number:'BigNumber'});
+
+function fancyEval(string) {
+  string = string.replace(_groupregex, '');
+  return math.eval(string).toString();
+};
+
 window.MoneyInput = React.createClass({
   getInitialState: function() {
     return {
@@ -5,23 +13,49 @@ window.MoneyInput = React.createClass({
     }
   },
   componentWillReceiveProps: function(props) {
-    var current = decimal2cents(this.state.display);
-    if (current !== props.value) {
+    var current = this.display2Cents(this.state.display);
+    if (current && current !== props.value) {
       // effectively different
       this.setState({display: cents2decimal(props.value) || ''})
     }
   },
+  display2Cents: function(display) {
+    try {
+      var answer = fancyEval(display);  
+    } catch(err) {
+    }
+    answer = answer || '';
+    return decimal2cents(answer);
+  },
   onDisplayChanged: function(e) {
     var display = e.target.value;
-    var value = decimal2cents(display);
+    var value = this.display2Cents(display);
     this.setState({display: display}, function() {
       this.props.onChange(value);
     });
   },
   render: function() {
-    var { value, onChange, ...other } = this.props;
+    var { value, onChange, className, ...other } = this.props;
+    console.log('classNAme', className);
+    var computed_value_elem;
+    var computed_value = this.display2Cents(this.state.display);
+    var all_classes = (className || '') + ' money-input';
+    var input_classes = className || '';
+    if (computed_value !== decimal2cents(this.state.display)) {
+      var classes = (className || '') + ' computed-value';
+      input_classes += ' with-computed';
+      if (computed_value < 0) {
+        classes += ' negative';
+      }
+      computed_value = cents2decimal(computed_value);
+      computed_value_elem = (<div className={classes}>{computed_value}</div>);
+    }
+
     return (
-      <input type="text" value={this.state.display} onChange={this.onDisplayChanged} {...other} />
+      <div {...other} className={all_classes} >
+        <input type="text" value={this.state.display} onChange={this.onDisplayChanged} {...other} className={input_classes} />&nbsp;
+        {computed_value_elem}
+      </div>
     );
   }
 });
