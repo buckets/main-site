3c406728944de479f0c7cc6a1eaf93f2859425f1
Matt Haggard <haggardii@gmail.com>
2016-07-28 23:04:24 -0600

    Starting make it rain

--- buckets/web/static/js/react-goals.jsx
+++ buckets/web/static/js/react-goals.jsx
@@ -2,9 +2,16 @@ window.GoalBar = React.createClass({
   render: function() {
     var { max, value, ...other } = this.props;
     var percent = value / max;
+    if (!isFinite(percent)) {
+      percent = 0;
+    }
     var display_percent = Math.round(100 * percent) + '%';
-    percent = percent > 1 ? 1 : percent;
-    percent = percent < 0 ? 0 : percent;
+    if (percent > 1) {
+      percent = 1;
+      display_percent = '100%+';
+    } else if (percent < 0) {
+      percent = 0;
+    }
     var width = Math.round(100 * percent) + '%';
     var forestyle = {
       width: width,
