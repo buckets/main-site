56ec3f9c0b1f5fe00058973190ba850926801493
Matt Haggard <haggardii@gmail.com>
2017-07-13 10:10:25 -0600

    posted dates a little more accurate

--- buckets/export.py
+++ buckets/export.py
@@ -1,16 +1,35 @@
 import sqlite3
 import os
 import shutil
-#import sqlalchemy
+import time
+import calendar
+from datetime import timedelta
 from sqlalchemy import select, and_
-#, and_, func, text
 
 from buckets.schema import Account, AccountTrans, Group, Bucket, BucketTrans
+from buckets.schema import Connection, AccountMapping
 
 project_root = os.path.join(os.path.dirname(__file__), '..')
 
 def convertTimestamp(x):
     return x.replace(microsecond=0)
+
+SECONDS_IN_A_DAY = 60 * 60 * 24
+utc_offset = calendar.timegm(time.gmtime()) - calendar.timegm(time.localtime())
+
+def convertPosted(posted, created):
+    if posted.hour == 0 and posted.minute == 0 and posted.second == 0:
+        # probably a fake time
+        if abs((posted - created).total_seconds()) <= SECONDS_IN_A_DAY:
+            # pretty close, use the created timestamp
+            return convertTimestamp(created)
+        else:
+            # different day
+            return convertTimestamp(posted + timedelta(seconds=utc_offset))
+    else:
+        return convertTimestamp(posted)
+
+
 
 def exportSqlite(engine, farm_id, sqlite_filename):
     base_buckets_files = os.path.join(project_root, 'porting/base.buckets')
@@ -41,7 +60,7 @@ def exportSqlite(engine, farm_id, sqlite_filename):
         ''', (
             trans.id,
             convertTimestamp(trans.created),
-            convertTimestamp(trans.posted),
+            convertPosted(trans.posted, trans.created),
             trans.account_id,
             trans.amount,
             trans.memo,
@@ -51,7 +70,7 @@ def exportSqlite(engine, farm_id, sqlite_filename):
 
     # bucket group
     r = engine.execute(select([Group])
-        .where(Group.farm_id == farm_id))
+        .where(Group.c.farm_id == farm_id))
     for group in r.fetchall():
         conn.execute('''
         INSERT INTO bucket_group
@@ -66,7 +85,7 @@ def exportSqlite(engine, farm_id, sqlite_filename):
 
     # buckets
     r = engine.execute(select([Bucket])
-        .where(Bucket.farm_id == farm_id))
+        .where(Bucket.c.farm_id == farm_id))
     buckets = list(r.fetchall())
     for bucket in buckets:
         kind = ''
@@ -79,6 +98,8 @@ def exportSqlite(engine, farm_id, sqlite_filename):
                 kind = 'goal-deposit'
             elif bucket.deposit and bucket.end_date:
                 kind = 'deposit-date'
+            else:
+                kind = 'goal-date'
         conn.execute('''
         INSERT INTO bucket
         (id, created, name, notes, balance, kicked, group_id, ranking, kind, goal, end_date, deposit, color)
@@ -105,14 +126,61 @@ def exportSqlite(engine, farm_id, sqlite_filename):
             BucketTrans.c.bucket_id == Bucket.c.id,
             Bucket.c.farm_id == farm_id)))
     for trans in r.fetchall():
-        MATT, you're right here
+        conn.execute('''
+        INSERT INTO bucket_transaction
+        (id, created, posted, bucket_id, amount, memo, account_trans_id, transfer)
+        VALUES (?, ?, ?, ?, ?, ?, ?, 0)
+        ''', (
+            trans.id,
+            convertTimestamp(trans.created),
+            convertPosted(trans.posted, trans.created),
+            trans.bucket_id,
+            trans.amount,
+            trans.memo,
+            trans.account_transaction_id,
+        ))
+
+    # simplefin connection
+    r = engine.execute(select([Connection])
+        .where(Connection.c.farm_id == farm_id))
+    for connection in r.fetchall():
+        conn.execute('''
+        INSERT INTO simplefin_connection
+        (id, created, access_token, last_used)
+        VALUES (?, ?, ?, ?)
+        ''', (
+            connection.id,
+            convertTimestamp(connection.created),
+            connection.access_token,
+            convertTimestamp(connection.last_used),
+        ))
+
+    # account mapping
+    r = engine.execute(select([AccountMapping])
+        .where(AccountMapping.c.farm_id == farm_id))
+    for mapping in r.fetchall():
+        conn.execute('''
+        INSERT INTO simplefin_connection
+        (id, created, account_id, account_hash)
+        VALUES (?, ?, ?, ?)
+        ''', (
+            mapping.id,
+            convertTimestamp(mapping.created),
+            mapping.account_id,
+            mapping.account_hash,
+        ))        
+
 
     # adjust balances
     # account
-    # bucket
+    for account in accounts:
+        conn.execute('UPDATE account SET balance=? WHERE id=?',
+            (account.balance, account.id))
 
-    import sys
-    sys.stdout.flush()
+    # bucket
+    for bucket in buckets:
+        conn.execute('UPDATE bucket SET balance=? WHERE id=?',
+            (bucket.balance, bucket.id))
 
     conn.commit()
     conn.close()
