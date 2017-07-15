95a420d92560dbd3cc20ac4a4b897ec031cd5354
Matt Haggard <haggardii@gmail.com>
2017-07-14 21:30:37 -0600

    Remove console.log

--- app/src/mainprocess/finding.ts
+++ app/src/mainprocess/finding.ts
@@ -1,19 +1,16 @@
 import {BrowserWindow} from 'electron';
 
 export function startFindInPage() {
-  console.log('findStart');
   let win = BrowserWindow.getFocusedWindow();
   win.webContents.send('find-start');
 }
 
 export function findNext() {
-  console.log('findNext');
   let win = BrowserWindow.getFocusedWindow();
   win.webContents.send('find-next');
 }
 
 export function findPrev() {
-  console.log('findPrev');
   let win = BrowserWindow.getFocusedWindow();
   win.webContents.send('find-prev');
 }
