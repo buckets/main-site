a309ce0706d18a4c0b04fbc9824d559eba18e6bf
Matt Haggard <haggardii@gmail.com>
2017-07-22 21:49:58 -0600

    Initial OFX parsing is okay

--- app/src/ofx.test.ts
+++ app/src/ofx.test.ts
@@ -1,5 +1,6 @@
 import { test } from 'tap'
 import { ofx2importable } from './ofx'
+import * as moment from 'moment'
 
 
 test('sample1', async t => {
@@ -95,6 +96,38 @@ NEWFILEUID:NONE
 </OFX>`
   let transactions = await ofx2importable(data);
   t.equal(transactions.length, 5, "Should have 5 transactions");
+
+  let [t0, t1, t2, t3, t4] = transactions;
+  t.equal((new Set(transactions.map(x => x.account_label))).size, 1,
+      "Should all have the same account label")
+  t.equal((new Set(transactions.map(x => x.currency))).size, 1,
+      "Should all have the same currency")
+  t.equal(t0.currency, 'NZD')
+  
+  t.equal(t0.amount, 1990)
+  t.equal(t0.memo, 'Credit Interest Paid')
+  t.equal(t0.fi_id, '20120928.0')
+  t.equal(t0.posted.format(), moment.utc({y:2012,M:9-1,d:28}).format())
+
+  t.equal(t1.amount, -101)
+  t.equal(t1.memo, 'Withholding Tax')
+  t.equal(t1.fi_id, '20120928.1')
+  t.equal(t1.posted.format(), moment.utc({y:2012,M:9-1,d:28}).format())
+
+  t.equal(t2.amount, -10000)
+  t.equal(t2.memo, 'Debit Transfer: Bike')
+  t.equal(t2.fi_id, '20120907.0')
+  t.equal(t2.posted.format(), moment.utc({y:2012,M:9-1,d:7}).format())
+
+  t.equal(t3.amount, -7199)
+  t.equal(t3.memo, 'Debit Transfer: Postage')
+  t.equal(t3.fi_id, '201209071')
+  t.equal(t3.posted.format(), moment.utc({y:2012,M:9-1,d:7}).format())
+
+  t.equal(t4.amount, -2001)
+  t.equal(t4.memo, 'Debit Transfer Party')
+  t.equal(t4.fi_id, '20120104.6')
+  t.equal(t4.posted.format(), moment.utc({y:2012,M:1-1,d:4}).format())
 })
 
 test('sample2', async t => {
@@ -166,4 +199,16 @@ NEWFILEUID:NONE
 </OFX>`
   let transactions = await ofx2importable(data);
   t.equal(transactions.length, 1)
+
+  let t0 = transactions[0];
+  t.notEqual(t0.account_label, null)
+  t.ok(t0.account_label.indexOf('55555555~1') !== -1, "Should have account number");
+  t.ok(t0.account_label.indexOf('GOOBYGOO') !== -1, "Should have bank");
+  t.ok(t0.account_label.indexOf('SAVINGS') !== -1, "Should have account name");
+  
+  t.equal(t0.currency, 'USD')
+  t.equal(t0.amount, 17)
+  t.equal(t0.memo, 'DIVIDEND EARNED FOR PERIOD OF 04/01/2016 THROUGH 04/30/2016 ANNUAL PERCENTAGE YIELD EARNED IS 0.10%')
+  t.equal(t0.fi_id, '0000760')
+  t.equal(t0.posted.format(), moment.utc({y:2016, M:4-1, d:30, h:12}).format())
 })
--- app/src/ofx.ts
+++ app/src/ofx.ts
@@ -1,14 +1,25 @@
 import * as _ from 'lodash'
-import { Timestamp } from './time'
+import * as moment from 'moment'
 import { parse as parseOFX } from 'ofx-js'
+import { decimal2cents } from './money'
 
 interface ImportableTrans {
   account_label: string;
   amount:number;
   memo:string;
-  posted:Timestamp;
+  posted:moment.Moment;
   fi_id?:string;
   currency?:string;
+}
+
+let formats = [
+  'YYYYMMDDHHmmss.SSS',
+  'YYYYMMDDHHmmss',
+  'YYYYMMDD',
+]
+function parseOFXDate(x:string):moment.Moment {
+  let ret = moment.utc(x, formats);
+  return ret;
 }
 
 export async function ofx2importable(ofx:string):Promise<ImportableTrans[]> {
@@ -24,7 +35,7 @@ export async function ofx2importable(ofx:string):Promise<ImportableTrans[]> {
   let account_description = '';
   let statement = parsed.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS;
   if (statement.CURDEF) {
-    currency = statement.CURDEF.toLowerCase();
+    currency = statement.CURDEF;
   }
   if (statement.BANKACCTFROM) {
     let acc = statement.BANKACCTFROM;
@@ -37,10 +48,18 @@ export async function ofx2importable(ofx:string):Promise<ImportableTrans[]> {
     } else {
       transactions = [statement.BANKTRANLIST.STMTTRN];
     }
-    console.log('transactions', transactions);
     ret = transactions.map((trans):ImportableTrans => {
+      let account_label = account_description;
+      if (fi_description) {
+        account_label += ' ' + fi_description;
+      }
       return {
-        hey matt, you're right here
+        account_label: account_label,
+        amount: decimal2cents(trans.TRNAMT),
+        memo: trans.MEMO || trans.NAME,
+        posted: parseOFXDate(trans.DTPOSTED),
+        fi_id: trans.FITID,
+        currency: currency,
       };
     })
   }
