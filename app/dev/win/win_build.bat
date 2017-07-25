fecaf766129d2e17e92c25bf6496c24533abc7c3
Matt Haggard <haggardii@gmail.com>
2017-07-25 14:06:10 -0600

    If I could just get yarn to finish...

--- app/dev/win/win_build.bat
+++ app/dev/win/win_build.bat
@@ -6,13 +6,14 @@ copy x:\package.json c:\cache\
 copy x:\yarn.lock c:\cache\
 cd \cache
 set NODE_PATH=c:\cache\node_modules
+setx NODE_PATH c:\cache\node_modules
 set PYTHON=C:\Users\IEUser\.windows-build-tools\python27\python.exe
+setx PYTHON C:\Users\IEUser\.windows-build-tools\python27\python.exe
 set PATH=%PATH%;C:\Users\IEUser\.windows-build-tools\python27
+setx PATH "%PATH%;C:\Users\IEUser\.windows-build-tools\python27"
 set
-call npm install -g node-gyp
-echo Gonna yarn now
-yarn
+call npm install -g node-gyp && call yarn && echo done
 
 x:
-yarn compile
+call yarn compile
 c:\cache\node_modules\.bin\build --win
