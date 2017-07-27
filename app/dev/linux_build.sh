8546a72de2d55cad105d0d72887591d5fa7b1860
Matt Haggard <haggardii@gmail.com>
2017-07-27 13:13:34 -0600

    Build linux without publishing

--- app/dev/linux_build.sh
+++ app/dev/linux_build.sh
@@ -3,10 +3,18 @@
 
 set -e
 
+CMD="$1"
+
+ARGS=""
+
+if [ "$CMD" == "publish" ]; then
+    ARGS="--env GH_TOKEN=$GH_TOKEN"
+fi
+
 TAG="buckets/linuxbuilder"
 echo "BUILDING..."
 docker build --file dev/linuxbuilder.Dockerfile -t $TAG .
 
 echo
 echo "RUNNING electron build..."
-docker run -i -v $(pwd):/code --env GH_TOKEN="$GH_TOKEN" $TAG
+docker run -i -v $(pwd):/code $ARGS $TAG
