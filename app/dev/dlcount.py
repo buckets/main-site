09ae404e1d1954b5f82ac65de75ae89798e616bc
Matt Haggard <haggardii@gmail.com>
2017-08-03 10:03:05 -0600

    Add script for download stats

---
+++ app/dev/dlcount.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import requests
+import os
+
+
+GH_TOKEN = os.environ['GH_TOKEN']
+GH_USER = os.environ.get('GH_USERNAME', 'iffy')
+
+# Get the most recent N releases
+number = 5
+r = requests.get('https://api.github.com/repos/buckets/application/releases',
+    auth=(GH_USER, GH_TOKEN))
+releases = r.json()
+for release in releases[:number]:
+    name = release['name']
+    dlcount = {
+        'win': 0,
+        'mac': 0,
+        'linux': 0,
+        'other': 0,
+    }
+    for asset in release['assets']:
+        asset_name = asset['name']
+        key = 'other'
+        if 'mac' in asset_name:
+            key = 'mac'
+        elif asset_name.endswith('.dmg'):
+            key = 'mac'
+        elif asset_name.endswith('.exe'):
+            key = 'win'
+        elif asset_name.endswith('.deb'):
+            key = 'linux'
+        dlcount[key] += asset['download_count']
+    print('{version} tot={total} win={win} mac={mac} linux={linux} other={other}'.format(
+        version=name,
+        win=dlcount['win'],
+        mac=dlcount['mac'],
+        linux=dlcount['linux'],
+        other=dlcount['other'],
+        total=sum(dlcount.values())))
