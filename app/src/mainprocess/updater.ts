7ab81f491918a9b18fffc11cf55bb88f60126ed9
Matt Haggard <haggardii@gmail.com>
2017-08-04 10:35:22 -0600

    Add auto-updating to linux

--- app/src/mainprocess/updater.ts
+++ app/src/mainprocess/updater.ts
@@ -1,5 +1,7 @@
 import * as log from 'electron-log'
-import { app, dialog } from 'electron'
+import * as electron_is from 'electron-is'
+import * as https from 'https'
+import { shell, app, dialog } from 'electron'
 import { autoUpdater } from 'electron-updater'
 
 export function checkForUpdates() {
@@ -16,7 +18,7 @@ export function checkForUpdates() {
     let new_version = info.version;
     dialog.showMessageBox({
       title: 'Update Available',
-      message: 'A free update is available for Buckets',
+      message: 'An update is available for Buckets',
       detail: `Current version: ${app.getVersion()}\nNew version: ${new_version}`,
       buttons: [
         'Later',
@@ -39,5 +41,61 @@ export function checkForUpdates() {
       autoUpdater.quitAndInstall();
     }
   });
-  autoUpdater.checkForUpdates();
+
+  if (electron_is.linux()) {
+    linux_checkForUpdates();
+  } else {
+    autoUpdater.checkForUpdates();
+  }
+}
+
+
+export async function linux_checkForUpdates() {
+  let new_version = await new Promise((resolve, reject) => {
+    https.get({
+      hostname: 'api.github.com',
+      port: 443,
+      path: '/repos/buckets/application/releases/latest',
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Buckets App (hello@bucketsisbetter.com)',
+      }
+    }, res => {
+      let payload = '';
+      res.setEncoding('utf8');
+      res.on('data', d => {
+        payload += d;
+      })
+      res.on('end', () => {
+        try {
+          const data = JSON.parse(payload);
+          resolve(data.tag_name);
+        } catch(err) {
+          log.error(`Error parsing payload: ${err.stack}`)
+          resolve(null);
+        }
+      })
+    }).on('error', err => {
+      log.error(`Error checking for update: ${err}`)
+      resolve(null);
+    })
+  });
+  let current_version = `v${app.getVersion()}`;
+  if (new_version && current_version !== new_version) {
+    dialog.showMessageBox({
+      title: 'Update Available',
+      message: 'An update is available for Buckets',
+      detail: `Current version: ${current_version}\nNew version: ${new_version}`,
+      buttons: [
+        'Later',
+        'Download',
+      ],
+      defaultId: 0,
+    }, (indexClicked) => {
+      if (indexClicked === 1) {
+        // Download
+        shell.openExternal('https://github.com/buckets/application/releases/latest')
+      }
+    })
+  }
 }
