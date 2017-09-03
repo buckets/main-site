967f9d335fdd77e4241485e7d19f983c65eb9fa1
Matt Haggard <haggardii@gmail.com>
2017-09-02 22:35:40 -0600

    Got English where it should be

--- app/src/langs/en.tsx
+++ app/src/langs/en.tsx
@@ -3,11 +3,8 @@ import { ILangPack } from './spec';
 export const pack:ILangPack = {
   name: 'English',
   messages: {
-    // Generic labels
     'labels.Trial Version': 'Trial Version',
     'labels.Rain': 'Rain',
-
-    // Nav bar
     'nav.Accounts': 'Accounts',
     'nav.Transactions': 'Transactions',
     'nav.Buckets': 'Buckets',
@@ -17,29 +14,22 @@ export const pack:ILangPack = {
     'nav.Connections': 'Connections',
     'nav.Import': 'Import',
     'nav.Chat with Matt': 'Chat with Matt',
-
-    // Menu items
     'menu.file.label': 'File',
     'menu.file.NewBudget': 'New Budget...',
     'menu.file.OpenBudget': 'Open Budget...',
     'menu.file.OpenRecent': 'Open Recent',
     'menu.file.Duplicate Window': 'Duplicate Window',
     'menu.file.ImportTransactions': 'Import transactions...',
-
     'menu.register.PurchaseFullVersion': 'Purchase Full Version...',
     'menu.register.EnterLicense': 'Enter License...',
-    
     'menu.help.LearnMore': 'Learn More',
     'menu.help.ShowLogFiles': 'Show Log Files...',
     'menu.help.ReportBug': 'Report Bug...',
     'menu.help.Chat': 'Chat...',
-    
-    // Wizard
     'wizard.Recently used': 'Recently used',
-
-    // Accounts
     "accounts.balance_mismatch_msg": 'The most recent synced balance does not match the balance computed from transactions.  Click more for more information.',
     "accounts.name_placeholder": "no name",
     "accounts.more_link": "more",
+    "accounts.Account": "Account",
   },
 }
