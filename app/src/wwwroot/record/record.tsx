9f704a73634f4531a24018bdde310bebcbd43933
Matt Haggard <haggardii@gmail.com>
2017-10-02 16:05:54 -0600

    Webview in react will probably work

--- app/src/wwwroot/record/record.tsx
+++ app/src/wwwroot/record/record.tsx
@@ -3,6 +3,53 @@ import { Renderer } from '../../budget/render'
 import { RecordingDirector } from '../../recordlib'
 import { sss } from '../../i18n'
 
+class Webview extends React.Component<any, any> {
+  private elem:Electron.WebviewTag;
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return <webview ref={elem => {
+      console.log('ref', elem);
+      if (elem) {
+        this.elem = elem;
+        elem.src = 'https://www.google.com';
+      }
+    }}
+      is
+      autosize="on"
+      disableguestresize
+      style={{width: '100%'}}
+      partition="persist:recordtest2" />
+  }
+}
+
+interface BrowserProps {
+  preload?: string;
+}
+class Browser extends React.Component<BrowserProps, {
+  foo: string;
+}> {
+  private webview:Electron.WebviewTag;
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: '',
+    }
+  }
+  render() {
+    return <div style={{width:'100%'}}>
+      <div>
+        React-controlled
+        <button onClick={() => {
+          this.setState({foo: 'something' + Math.random()});
+        }}>foo {this.state.foo}</button>
+      </div>
+      <Webview />
+    </div>
+  }
+}
+
 interface HeaderProps {
   webview: Electron.WebviewTag;
   url: string;
@@ -120,102 +167,97 @@ class Header extends React.Component<HeaderProps, {
 
 
 
-export function start(control, webview:Electron.WebviewTag) {
+export function start(container, preload_url:string) {
   const renderer = new Renderer();
   
-  const director = new RecordingDirector(webview);
-  director.on('recorded-step', () => {
-    renderer.doUpdate();
-  })
+  // const director = new RecordingDirector(webview);
+  // director.on('recorded-step', () => {
+  //   renderer.doUpdate();
+  // })
 
   renderer.registerRendering(() => {
-    const url = webview.getWebContents && webview.getWebContents() ? webview.getURL() : '';
-    return <Header
-      webview={webview}
-      url={url}
-      director={director}
-      renderer={renderer}
-    />
-  }, control);
+    // const url = webview.getWebContents && webview.getWebContents() ? webview.getURL() : '';
+    return <Browser preload={preload_url} />
+  }, container);
 
 
-  const events = [
-    // 'load-commit',
-    // 'did-finish-load',
-    // 'did-fail-load',
-    // 'did-frame-finish-load',
-    // 'did-start-loading',
-    // 'did-stop-loading',
-    // 'did-get-response-details',
-    // 'did-get-redirect-request',
-    // 'dom-ready',
-    // // 'page-title-updated',
-    // // 'page-favicon-updated',
-    // // 'enter-html-full-screen',
-    // // 'leave-html-full-screen',
-    // // 'console-message',
-    // 'found-in-page',
-    // 'new-window',
-    // 'will-navigate',
-    // // 'did-navigate',
-    // 'did-navigate-in-page',
-    // 'close',
-    // // 'ipc-message',
-    // 'crashed',
-    // 'gpu-crashed',
-    // 'plugin-crashed',
-    // 'destroyed',
-    // // 'media-started-playing',
-    // // 'media-paused',
-    // // 'did-change-theme-color',
-    // // 'update-target-url',
-    // // 'devtools-opened',
-    // // 'devtools-closed',
-    // // 'devtools-focused',
-  ];
-  events.forEach(event => {
-    webview.addEventListener(event as any, (ev) => {
-      console.log('event', event, ev);
-    }, false);
-  })
-
-  webview.addEventListener('console-message', (ev) => {
-    // console.log('webview:', ev.message);
-  }, false)
-  webview.addEventListener('page-favicon-updated', (ev) => {
-    // console.log('favicon', ev);
-    // if (ev.favicons.length) {
-    //   let favicon = ev.favicons[0];
-    // } else {
-
-    // }
-  })
-  webview.addEventListener('did-navigate', (ev) => {
-    console.log('did-navigate');
-    renderer.doUpdate();
-  }, false);
-  webview.addEventListener('page-title-updated', (ev) => {
-    document.title = `[Experimental] Buckets Recorder - ${ev.title}`;
-  }, false);
-
-  webview.addEventListener('did-start-loading', () => {
-    renderer.doUpdate();
-  }, false);
-  webview.addEventListener('did-stop-loading', () => {
-    renderer.doUpdate();
-  }, false);
-
-  // webview.addEventListener('did-fail-load', () => {
-  //   console.log('did fail load');
+  // const events = [
+  //   // 'load-commit',
+  //   // 'did-finish-load',
+  //   // 'did-fail-load',
+  //   // 'did-frame-finish-load',
+  //   // 'did-start-loading',
+  //   // 'did-stop-loading',
+  //   // 'did-get-response-details',
+  //   // 'did-get-redirect-request',
+  //   // 'dom-ready',
+  //   // // 'page-title-updated',
+  //   // // 'page-favicon-updated',
+  //   // // 'enter-html-full-screen',
+  //   // // 'leave-html-full-screen',
+  //   // // 'console-message',
+  //   // 'found-in-page',
+  //   // 'new-window',
+  //   // 'will-navigate',
+  //   // // 'did-navigate',
+  //   // 'did-navigate-in-page',
+  //   // 'close',
+  //   // // 'ipc-message',
+  //   // 'crashed',
+  //   // 'gpu-crashed',
+  //   // 'plugin-crashed',
+  //   // 'destroyed',
+  //   // // 'media-started-playing',
+  //   // // 'media-paused',
+  //   // // 'did-change-theme-color',
+  //   // // 'update-target-url',
+  //   // // 'devtools-opened',
+  //   // // 'devtools-closed',
+  //   // // 'devtools-focused',
+  // ];
+  // events.forEach(event => {
+  //   webview.addEventListener(event as any, (ev) => {
+  //     console.log('event', event, ev);
+  //   }, false);
   // })
-  // webview.addEventListener('dom-ready', () => {
-  //   console.log('dom-ready webview');
+
+  // webview.addEventListener('console-message', (ev) => {
+  //   // console.log('webview:', ev.message);
   // }, false)
+  // webview.addEventListener('page-favicon-updated', (ev) => {
+  //   // console.log('favicon', ev);
+  //   // if (ev.favicons.length) {
+  //   //   let favicon = ev.favicons[0];
+  //   // } else {
+
+  //   // }
+  // })
+  // webview.addEventListener('did-navigate', (ev) => {
+  //   console.log('did-navigate');
+  //   renderer.doUpdate();
+  // }, false);
+  // webview.addEventListener('page-title-updated', (ev) => {
+  //   document.title = `[Experimental] Buckets Recorder - ${ev.title}`;
+  // }, false);
+
+  // webview.addEventListener('did-start-loading', () => {
+  //   renderer.doUpdate();
+  // }, false);
+  // webview.addEventListener('did-stop-loading', () => {
+  //   renderer.doUpdate();
+  // }, false);
+
+  // // webview.addEventListener('did-fail-load', () => {
+  // //   console.log('did fail load');
+  // // })
+  // // webview.addEventListener('dom-ready', () => {
+  // //   console.log('dom-ready webview');
+  // // }, false)
   
-  // while debugging
-  webview.src = 'http://127.0.0.1:8080';
-  director.startRecording();
-  director.setURL(webview.src);
+  // // while debugging
+  // webview.src = 'http://127.0.0.1:8080';
+  // director.startRecording();
+  // director.setURL(webview.src);
 
   renderer.doUpdate();
 }
