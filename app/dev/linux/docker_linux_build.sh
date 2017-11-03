1e0c3a4fba60cde574eaa25da34023e49243aa54
Matt Haggard <haggardii@gmail.com>
2017-11-03 14:27:19 -0600

    Fix linux building

--- app/dev/linux/docker_linux_build.sh
+++ app/dev/linux/docker_linux_build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # This is run on the linux container
 
+# copy to build_dir
+ORIG_DIR="$(pwd)"
+BUILD_DIR="/tmp/build_dir"
+
+mkdir -p "$BUILD_DIR"
+rsync -vrut \
+    --exclude "dist/" \
+    --exclude="node_modules/" \
+    "${ORIG_DIR}/" "$BUILD_DIR"
+
+pushd "$BUILD_DIR"
 export PATH="/cache/node_modules/.bin/:${PATH}"
 
 ARGS=""
@@ -9,3 +20,5 @@ if [ ! -z "$GH_TOKEN" ]; then
 fi
 
 build --linux $ARGS
+
+rsync -vrut "${BUILD_DIR}/dist/" "${ORIG_DIR}/dist"
