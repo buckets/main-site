e92a7cc069efa548920ee2aedd2bcf190c78bf27
Matt Haggard <haggardii@gmail.com>
2018-01-11 07:53:00 -0700

    Updateissuelabels.py doesn't need args now

--- app/dev/changelog/updateissuelabels.py
+++ app/dev/changelog/updateissuelabels.py
@@ -5,13 +5,21 @@ import sys
 import re
 import json
 import os
+import glob
 
 GH_TOKEN = os.environ['GH_TOKEN']
 GH_USER = os.environ.get('GH_USERNAME', 'iffy')
 
 r_issuenum = re.compile(r'#([0-9]+)')
 
-for filename in sys.argv[1:]:
+filenames = sys.argv[1:]
+if not filenames:
+    pattern = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../changes/') + '*.md')
+    print('pattern', pattern)
+    filenames = glob.glob(pattern)
+
+for filename in filenames:
+    print(filename)
     with io.open(filename, 'rb') as fh:
         nums = r_issuenum.findall(fh.read())
         for num in nums:
