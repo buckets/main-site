8252d751e325fbe3a879aa6c35b400261a67eef2
Matt Haggard <haggardii@gmail.com>
2018-03-07 21:56:12 -0700

    Rain tooltip no longer has faded cents. (#TlvT3o2a)

--- app/src/budget/budget.tsx
+++ app/src/budget/budget.tsx
@@ -218,17 +218,17 @@ class Application extends React.Component<ApplicationProps, any> {
                               <tr>
                                 <td>{sss('Accounts')}</td>
                                 <td></td>
-                                <td className="right"><Money value={appstate.account_total_balance} /></td>
+                                <td className="right"><Money value={appstate.account_total_balance} noFaintCents /></td>
                               </tr>
                               <tr>
                                 <td>{sss('Buckets')}</td>
                                 <td>-</td>
-                                <td className="right"><Money value={appstate.bucket_total_balance} /></td>
+                                <td className="right"><Money value={appstate.bucket_total_balance} noFaintCents /></td>
                               </tr>
                               <tr>
                                 <td>{sss('Used in future')}</td>
                                 <td>-</td>
-                                <td className="right"><Money value={appstate.adjusted_future_rain} /></td>
+                                <td className="right"><Money value={appstate.adjusted_future_rain} noFaintCents /></td>
                               </tr>
                               <tr>
                                 <td colSpan={3} className="total-line">
@@ -237,7 +237,7 @@ class Application extends React.Component<ApplicationProps, any> {
                               <tr>
                                 <td>{sss('Rain')}</td>
                                 <td></td>
-                                <td className="right"><Money value={appstate.rain} /></td>
+                                <td className="right"><Money value={appstate.rain} noFaintCents /></td>
                               </tr>
                               <tr>
                                 <td colSpan={3}>
@@ -251,7 +251,7 @@ class Application extends React.Component<ApplicationProps, any> {
                                       return <span>
                                       You have put {abs_amount} too much money into buckets.  If all transactions have been categorized this month, remove {abs_amount} from buckets of your choosing.
                                       </span>
-                                    })(<Money value={Math.abs(appstate.rain)} />)
+                                    })(<Money value={Math.abs(appstate.rain)} noFaintCents />)
                                   }
                                 </td>
                               </tr>
