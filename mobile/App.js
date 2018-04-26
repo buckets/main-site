0cd5d84e4ca64db829d875f8f27b797843fa6ba4
Matt Haggard <haggardii@gmail.com>
2018-04-26 15:34:23 -0600

    can access filesystem

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { dostuff } from './dist'
 
 export default class App extends React.Component {
   render() {
-    const a = dostuff();
-    console.log('a', a);
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={() => {
+            dostuff();
+          }}
+          title="Click me" />
       </View>
     );
   }
--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,18 +1,35 @@
-import { SQLite } from 'expo';
+import { SQLite, FileSystem, DocumentPicker } from 'expo';
 
-export function dostuff():string {
+export async function dostuff():string {
   console.log('stuff done');
 
+  // const result = await DocumentPicker.getDocumentAsync({
+  //   type: '*/*',
+  // })
+  // console.log('result', result);
+
   const db = SQLite.openDatabase("test.db");
-  db.transaction(tx => {
-    tx.executeSql(
-      'select sqlite_version()',
-      [],
-      (_, result) => {
-        console.log('result', result);
-        console.log('rows', result.rows._array);
-      }
-    )
+  await new Promise<any>((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'select sqlite_version()',
+        [],
+        (_, result) => {
+          console.log('result', result);
+          console.log('rows', result.rows._array);
+          resolve(null);
+        }
+      )
+    })
   })
-  return 'x'
+
+  try {
+    const info = await FileSystem.getInfoAsync(FileSystem.documentDirectory)
+    console.log("info", info);  
+  } catch(err) {
+    console.log('Error accessing file', err);
+  }
+
+  const items = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory)
+  console.log('items', items);
 }
