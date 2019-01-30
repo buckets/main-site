d546aed27af1f512f75564a042b7692ba8931d45
Matt Haggard <haggardii@gmail.com>
2019-01-30 12:51:56 -0700

    Forget tap

--- core/test.sh
+++ core/test.sh
@@ -3,7 +3,8 @@
 extraargs="$*"
 export TAP_RCFILE=".taprc"
 [ ! -d dist ] && tsc
-# for filename in $(find dist -name "*.test.js"); do
-#     node "$filename"
-# done
-find dist -name "*.test.js" | xargs tap $* --
+set -e
+for filename in $(find dist -name "*.test.js"); do
+    node "$filename"
+done
+# find dist -name "*.test.js" | xargs valgrind --tool=memcheck --leak-check=summary --show-leak-kinds=all tap -j 1 $* --
