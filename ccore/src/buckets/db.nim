f914752b787fba3e62fea63ebb59a105d237b8a0
Matt Haggard <haggardii@gmail.com>
2019-01-30 21:27:17 -0700

    Actually define things

--- ccore/src/buckets/db.nim
+++ ccore/src/buckets/db.nim
@@ -1,16 +1,16 @@
 {.compile: "./sqlite3/sqlite3.c" .}
-{.passC: "-dSQLITE_ENABLE_JSON1" .}
-{.passC: "-dSQLITE_THREADSAFE=0" .}
-{.passC: "-dSQLITE_DEFAULT_MEMSTATUS=0" .}
-{.passC: "-dSQLITE_DEFAULT_WAL_SYNCHRONOUS=1" .}
-{.passC: "-dSQLITE_LIKE_DOESNT_MATCH_BLOBS" .}
-{.passC: "-dSQLITE_MAX_EXPR_DEPTH=0" .}
-{.passC: "-dSQLITE_OMIT_DECLTYPE" .}
-{.passC: "-dSQLITE_OMIT_DEPRECATED" .}
-{.passC: "-dSQLITE_OMIT_PROGRESS_CALLBACK" .}
-{.passC: "-dSQLITE_OMIT_SHARED_CACHE" .}
-{.passC: "-dSQLITE_USE_ALLOCA" .}
-{.passC: "-dSQLITE_ENABLE_COLUMN_METADATA" .}
+{.passC: "-D SQLITE_ENABLE_JSON1" .}
+{.passC: "-D SQLITE_THREADSAFE=0" .}
+{.passC: "-D SQLITE_DEFAULT_MEMSTATUS=0" .}
+{.passC: "-D SQLITE_DEFAULT_WAL_SYNCHRONOUS=1" .}
+{.passC: "-D SQLITE_LIKE_DOESNT_MATCH_BLOBS" .}
+{.passC: "-D SQLITE_MAX_EXPR_DEPTH=0" .}
+# {.passC: "-D SQLITE_OMIT_DECLTYPE" .}
+{.passC: "-D SQLITE_OMIT_DEPRECATED" .}
+{.passC: "-D SQLITE_OMIT_PROGRESS_CALLBACK" .}
+{.passC: "-D SQLITE_OMIT_SHARED_CACHE" .}
+{.passC: "-D SQLITE_USE_ALLOCA" .}
+{.passC: "-D SQLITE_ENABLE_COLUMN_METADATA" .}
 
 import logging
 import sqlite3
