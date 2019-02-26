f588ff7ad50ea57fadf897d13d85135051127914
Matt Haggard <haggardii@gmail.com>
2019-02-25 21:09:25 -0700

    macOS beta build works

--- nakefile.nim
+++ nakefile.nim
@@ -126,16 +126,6 @@ task "core-test", "Run core/ tests":
 const
   APP_NB_LIB = "app"/"node_modules"/"bucketslib"/"lib"/"bucketslib.node"
 
-task "build-desktop", "Build a dev version of the app":
-  runTask "desktop-js"
-  withDir("app"):
-    when defined(windows):
-      direShell "build", "--win", "--x64", "--ia32"
-    elif defined(macosx):
-      direShell "build", "--mac"
-    else:
-      direShell "build", "--linux"
-
 task "desktop-lib", "Refresh app/ nodebuckets library":
   runTask "core-js"
   if APP_NB_LIB.needsRefresh(CORE_NB_LIB):
@@ -158,3 +148,67 @@ task "desktop-test", "Run app/ tests":
   
   withDir("app"):
     direShell "yarn", "test"
+
+var
+  BUILD_BETA = false
+
+proc currentDesktopVersion():string =
+  for line in readFile("app"/"package.json").splitLines():
+    if "version" in line:
+      result = line.split("\"")[3]
+      break
+
+template assertGitHubToken():untyped =
+  assert(getEnv("GH_TOKEN", "") != "", "You must set the GH_TOKEN environment variable.")
+
+task "build-desktop", "Build a dev version of the app":
+  runTask "desktop-js"
+  var args = @["build"]
+  if BUILD_BETA:
+    args.add(@["--config", "config_beta.js"])
+  withDir("app"):
+    when defined(windows):
+      args.add("--win", "--x64", "--ia32")
+    elif defined(macosx):
+      args.add("--mac")
+    else:
+      args.add("--linux")
+    direShell(args)
+
+task "build-desktop-beta", "Build the desktop beta app":
+  BUILD_BETA = true
+  runTask "build-desktop"
+
+task "publish-desktop-mac", "Publish macOS desktop app to GitHub":
+  assertGitHubToken()
+  let
+    old_csc_link = getEnv("CSC_LINK", "")
+    old_csc_password = getEnv("CSC_KEY_PASSWORD", "")
+  var args = @["node_modules"/".bin"/"build", "--mac", "-p", "always"]
+  if BUILD_BETA:
+    args.add(@["--config", "config_beta.js"])
+  try:
+    putEnv("CSC_LINK", "")
+    putEnv("CSC_KEY_PASSWORD", "")
+    withDir("app"):
+      direShell(args)
+  finally:
+    putEnv("CSC_LINK", old_csc_link)
+    putEnv("CSC_KEY_PASSWORD", old_csc_password)
+
+task "publish-desktop-linux", "Publish Linux desktop app to GitHub":
+  assertGitHubToken()
+
+task "publish-desktop-windows", "Publish Windows desktop app to GitHub":
+  assertGitHubToken()
+
+when defined(macosx):
+  task "publish-all-desktop-beta", "Build and publish a new beta version":
+    BUILD_BETA = true
+    runTask "publish-all-desktop"
+
+when defined(macosx):
+  task "publish-all-desktop", "Build and publish a new version of the desktop app":
+    runTask "publish-desktop-mac"
+    runTask "publish-desktop-linux"
+    runTask "publish-desktop-windows"
